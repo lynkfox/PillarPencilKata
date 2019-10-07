e514d2ebe8186427a4ae18716c9dbe820f0f8936
Lynkfox <lynkfox@gmail.com>
2019-10-06 21:06:54 -0400

    Refactor

--- PencilSimulationSolution/PencilTests/PencilTests.cs
+++ PencilSimulationSolution/PencilTests/PencilTests.cs
@@ -7,19 +7,21 @@ namespace PencilTests
     [TestClass]
     public class PencilTests
     {
+
+        public static int tipDurability = 20;
+        public static Pencil pencil = new Pencil(tipDurability);
+
         [TestMethod]
         public void PencilTipDurabilityIs20()
         {
-            int durability = 20;
-            Pencil pencil = new Pencil(durability);
-            Assert.AreEqual(durability, pencil.tip);
+            Assert.AreEqual(20, pencil.tip);
         }
 
           [TestMethod]
           public void PencilWriteReturnsString()
         {
             string input = "Test";
-            Pencil pencil = new Pencil(20);
+            
             Assert.AreEqual("Test", pencil.write(input));
 
         }
