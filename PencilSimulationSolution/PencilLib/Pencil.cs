371927183ce0bee69e0a10c03d92e0f467ada3ff
Lynkfox <lynkfox@gmail.com>
2019-10-07 07:15:13 -0400

    Refactoring
    Notes, Comments, and some name small changes

--- PencilSimulationSolution/PencilLib/Pencil.cs
+++ PencilSimulationSolution/PencilLib/Pencil.cs
@@ -6,28 +6,39 @@ namespace PencilLib
     {
         /* CONST default vaules of Pencil
          */
-
         private const int LENGTH = 40;
         private const int TIP = 20;
+        private const int ERASER = 60;
+
+
+
         /* Private Internal variables
          */
         private int maxDurability;
 
+
+
         /* Public Variables
          */
-
         public int Tip { get; set; }
         public int Length { get; set; }
         public int Eraser { get; set; }
 
 
-
+        public Pencil()
+        {
+            this.maxDurability = TIP;
+            this.Tip = TIP;
+            this.Length = LENGTH;
+            this.Eraser = ERASER;
+        }
 
         public Pencil(int tipStartingValue)
         {
             maxDurability = tipStartingValue;
             this.Tip = maxDurability;
             this.Length = LENGTH;
+            this.Eraser = ERASER;
         }
 
         public Pencil(int tipDurability, int length)
@@ -35,6 +46,7 @@ namespace PencilLib
             this.maxDurability= tipDurability;
             this.Tip = this.maxDurability;
             this.Length = length;
+            this.Eraser = ERASER;
         }
 
         public Pencil(int tipDurability, int length, int eraserDurability)
@@ -45,12 +57,29 @@ namespace PencilLib
             this.Eraser = eraserDurability;
         }
 
+
+        /* Function : Reduces the Tip Durability by the cost of the input. 
+         * 
+         * Returns the word for use later.
+         * 
+         * TO DO : What if Tip does not have enough durability?
+         */
         public string Write(string word)
         {
             this.Tip -= TipDurabilityLoss(word);
             return word;
         }
 
+        /* Function: Determines the Durability Cost of Writing using the following requriements:
+         * 
+         * Capital Letter - 2pts
+         * Lower Case Letter - 1pt
+         * White Space = 0pts
+         * 
+         * Other Characters = 1pt;
+         * 
+         * TO DO: Move to Private function (not Needed outside of Pencil)
+         */
         public int TipDurabilityLoss(string input)
         {
             int totalDurabilityCost = 0;
@@ -58,7 +87,7 @@ namespace PencilLib
             {
                 if(char.IsUpper(letter))
                 {
-                    //Capital Letters increase durability cost by 1
+                    //Capital Letters increase durability cost by 2
                     totalDurabilityCost += 2;
                 }else if (letter != ' ' )
                 {
@@ -70,12 +99,26 @@ namespace PencilLib
             return totalDurabilityCost;
         }
 
+
+        /* Function - Returns Pencil to max Sharpness while reducing Length.
+         * 
+         * TO DO : What if Length == 0?
+         */
         public void Sharpen()
         {
             this.Length -= 1;
             Tip = maxDurability;
         }
 
+
+        /* Function : Determines the Durability loss of input for Erasing a word based on the following criteria:
+         * 
+         * Letter (Capital or Lower) - 1pt
+         * White Space - 0pt
+         * All other Characters - 1pt
+         *
+         * To Do : Make Private (Not Needed outside of Pencil)
+         */
         public int EraserDurabilityLoss(string input)
         {
             int durabilityPoints = 0;
